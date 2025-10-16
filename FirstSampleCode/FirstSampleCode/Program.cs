f3850dce36c5a004fcb184682d54129d00fb2511
alamdilshad87 <dilshadand@gmail.com>
2025-10-16 15:22:03 +0530

    First Code

---
+++ FirstSampleCode/FirstSampleCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstSampleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! \nThis side Md Dilshad Alam");
+        }
+    }
+}
