062f0916dadd0cb7677b8069018f090d379a89e6
alamdilshad87 <dilshadand@gmail.com>
2025-10-17 14:26:26 +0530

    Datatypes

---
+++ Datatypes_Questions/Datatypes_Questions/Explicit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Datatypes_Questions
+{
+    class Explicit
+    {
+        static void Main(string[] args)
+        {
+            int num = 65;
+            char alpha;
+            alpha = (char)num;
+            Console.WriteLine($"alphabet is: {alpha}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Datatypes_Questions/Datatypes_Questions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Datatypes_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20000;
+            int num2 = 50000;
+            long total;
+            total = num1 + num2;
+
+            Console.WriteLine($"Total is: {total}");
+            Console.ReadLine();
+        }
+    }
+}
